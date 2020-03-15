5079eca8ea3a11f3fd9e7bc4bcb1220afd4f35de
keylesssky187 <60062880+keylesssky187@users.noreply.github.com>
2020-03-15 14:40:01 +0300

    vue 2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let = vm = new Vue({
+let vm = new Vue({
 	el: "#app",
 	data:{
-		message: "Hello Vue js"
+		message: "Hello Vue js",
+		mes: "first",
+	    checked: false,
+	    p: "First",
+	    language: "Js",
+	    n: 3,
+	    m: "Строка",
+	    query: "Find",
 	}
 })
